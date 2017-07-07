a4db9c308e76ab45f21790b1eea95782413f3d15
root <ankit.june25@gmail.com>
2017-07-08 01:05:47 +0530

    Add SOP to ChargerClient.java

--- DesignWork/src/pr/practice/design/adapter/ChargerClient.java
+++ DesignWork/src/pr/practice/design/adapter/ChargerClient.java
@@ -4,6 +4,7 @@ public class ChargerClient {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Adapt i4 charger as i5 charger.");
 		ChargePhone charge = new ChargePhone();
 		Charger charger = new i5ChargerAdapter();
 		charge.setChager(charger);
