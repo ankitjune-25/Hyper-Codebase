78d91555d85a4f29d309694b700ff0e776dd67e4
root <ankit.june25@gmail.com>
2017-07-08 00:16:50 +0530

    Add SOP to BuyerCLient.java

--- DesignWork/src/pr/practice/design/abstractFactory/BuyerClient.java
+++ DesignWork/src/pr/practice/design/abstractFactory/BuyerClient.java
@@ -4,6 +4,7 @@ public class BuyerClient {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Create object from factory.");
 		OrderFactoryProducer orderFactory = new OrderFactoryProducer();
 		orderFactory.placeOrder("Laptop").orderLaptop("Mac").os();
 		orderFactory.placeOrder("Car").orderCar("Safari").type();
